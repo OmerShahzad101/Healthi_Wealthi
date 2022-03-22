9e7c3665a4afeb55930217b22a157467e80540f0
Omer Shahzad <omershahzad707@gmail.com>
2022-03-22 20:03:23 +0500

    db configration

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+// const app = require("./config/express");
+// const mongoose = require("./config/mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/HealthiWealthi") , () => {
+//     con
+// }
+
+// app.listen(8080, () => {
+//   console.log("BE started at port 9002");
+// });
+
+// module.exports = app;
+
 const config = require('./config/vars');
 const app = require('./config/express');
 const routes = require('./routes/index');
 const mongoose = require('./config/mongoose');
 
-mongoose.connect();
+mongoose.connect("mongodb://localhost:27017/HealthiWealthi");
 
 // mount api v1 routes
 app.use('/api', routes);
