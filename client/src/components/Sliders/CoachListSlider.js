27f7e1570ee684d45e1051b2bb65367214ecdaab
Omer Shahzad <omershahzad707@gmail.com>
2022-03-22 19:39:35 +0500

    backend added

--- client/src/components/Sliders/CoachListSlider.js
+++ client/src/components/Sliders/CoachListSlider.js
@@ -29,7 +29,7 @@ const CoachListSlider = () => {
                 still in their infancy. Various versions have evolved over the
                 years, sometimes
               </p>
-              <a href="javascript:;">Read More..</a>
+              <a href="#">Read More..</a>
             </div>
           </div>
           <div class="col-lg-8">
